9feb1f2af85d5bc41b0c516a6a876c743c5cd05f
Randy0607 <randall.julies89@gmail.com>
2024-07-03 16:53:44 +0200

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,14 +8,14 @@ import sys
 if __name__ == '__main__':
 
     filesize, count = 0, 0
-    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    status_count = {k: 0 for k in status_codes}
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-    def print_stats(stats, file_size):
+    def print_stats(stats: dict, file_size: int) -> None:
         print("File size: {:d}".format(filesize))
-        for key, val in sorted(stats.items()):
-            if val:
-                print("{}: {}".format(key, val))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
@@ -23,19 +23,17 @@ if __name__ == '__main__':
             data = line.split()
             try:
                 status_code = data[-2]
-                if status_code in status_codes:
-                    status_count[status_code] += 1
+                if status_code in stats:
+                    stats[status_code] += 1
             except BaseException:
                 pass
             try:
                 filesize += int(data[-1])
             except BaseException:
                 pass
-            if count == 10:
-                count = 0
-                print_stats(status_count, filesize)
-        print_stats(status_count, filesize)
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        # Handle keyboard interruptioon (Ctrl+C) And Print final statistics
-        print_stats(status_count, filesize)
+        print_stats(stats, filesize)
         raise
