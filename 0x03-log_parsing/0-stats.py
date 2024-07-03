72de44793df5b724f06af902f4f78100d65c816a
Randy0607 <randall.julies89@gmail.com>
2024-07-03 17:01:22 +0200

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,100 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
 
+"""
+This script reads stdin line by line and computes metrics:
+
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+It prints the statistics of total lines read after reading 10 lines or keyboard
+interruption )CTRL + C)
+
+Output format:
+    -Total file size: File size: <total size>
+     where <total size>  is the sum of all previous <file size>
+
+    - Number of lines by status code:
+    - Possible status code:
+         - 200, 301, 400, 401, 403, 404, 405, 500
+
+    - Prints nothing if the status code doesnt appear
+      else prints in the format: <status code>: <number>
+
+    - Status codes are printed in ascending order
+"""
 import sys
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+class LogParser:
+    def __init__(self):
+        """
+        Initializes a new Instance of LogParser class
+        """
+        self.status_codes = ['200', '301', '400', '401',
+                             '403', '404', '405', '500']
+        self.total_size = 0
+        self.stats = {}
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+    def display_stats(self):
+        """
+           Displays summary of the stats read from stdin.
+        """
+        print("File size: {}".format(self.total_size))
+        for key in sorted(self.stats):
+            print("{}: {}".format(key, self.stats[key]))
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+    def parse_line(self, line):
+        """
+        Parses input line
+        line (string): The input line of log stream
+
+        Returns (dict): Dictionary with size of line,
+                        status code and
+                        validity of read status code
+        Raises: (IndexError) , (ValueError) when invalid
+                line format is encountered
+        """
+        line_parts = line.split()
+        try:
+            if line_parts[-2] in self.status_codes:
+                is_valid_code = True
+            else:
+                is_valid_code = False
+
+            return {
+                "line_size": int(line_parts[-1]),
+                "status_code": line_parts[-2],
+                "is_valid_code": is_valid_code
+            }
+        except (IndexError, ValueError):
+            return None
+
+    def read_from_stdin(self):
+        """
+        Reads from standard input and displays stats
+        after 10 lines or user interruption
+
+        Raises:
+            KeyboardInterrupt: If user presses (CTRL + C)
+        """
+        line_count = 0
+        try:
+            for line in sys.stdin:
+                line_count += 1
+                parsed_data = self.parse_line(line)
+
+                if parsed_data is not None:
+                    self.total_size += parsed_data["line_size"]
+                    if parsed_data["is_valid_code"]:
+                        total = self.stats.get(parsed_data["status_code"], 0)
+                        self.stats[parsed_data["status_code"]] = total + 1
+                if line_count % 10 == 0:
+                    self.display_stats()
+            self.display_stats()
+        except KeyboardInterrupt:
+            self.display_stats()
+
+
+if __name__ == "__main__":
+    parser = LogParser()
+    parser.read_from_stdin()
