87b4611c85b8f2c666530d049acaac8fcc92faa8
Randy0607 <randall.julies89@gmail.com>
2024-07-03 17:06:18 +0200

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,100 +1,68 @@
 #!/usr/bin/python3
+"""Write a script that reads stdin line by line and computes metrics:
 
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> (if the format is not this one, the line
+must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear or is not an integer,
+don’t print anything for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
+
+line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
+<status code>, <file size>]
 """
-This script reads stdin line by line and computes metrics:
 
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
 
-It prints the statistics of total lines read after reading 10 lines or keyboard
-interruption )CTRL + C)
-
-Output format:
-    -Total file size: File size: <total size>
-     where <total size>  is the sum of all previous <file size>
-
-    - Number of lines by status code:
-    - Possible status code:
-         - 200, 301, 400, 401, 403, 404, 405, 500
-
-    - Prints nothing if the status code doesnt appear
-      else prints in the format: <status code>: <number>
-
-    - Status codes are printed in ascending order
-"""
 import sys
 
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
-class LogParser:
-    def __init__(self):
-        """
-        Initializes a new Instance of LogParser class
-        """
-        self.status_codes = ['200', '301', '400', '401',
-                             '403', '404', '405', '500']
-        self.total_size = 0
-        self.stats = {}
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-    def display_stats(self):
-        """
-           Displays summary of the stats read from stdin.
-        """
-        print("File size: {}".format(self.total_size))
-        for key in sorted(self.stats):
-            print("{}: {}".format(key, self.stats[key]))
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
-    def parse_line(self, line):
-        """
-        Parses input line
-        line (string): The input line of log stream
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-        Returns (dict): Dictionary with size of line,
-                        status code and
-                        validity of read status code
-        Raises: (IndexError) , (ValueError) when invalid
-                line format is encountered
-        """
-        line_parts = line.split()
-        try:
-            if line_parts[-2] in self.status_codes:
-                is_valid_code = True
-            else:
-                is_valid_code = False
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
-            return {
-                "line_size": int(line_parts[-1]),
-                "status_code": line_parts[-2],
-                "is_valid_code": is_valid_code
-            }
-        except (IndexError, ValueError):
-            return None
+            # update total size
+            total_size += file_size
 
-    def read_from_stdin(self):
-        """
-        Reads from standard input and displays stats
-        after 10 lines or user interruption
+            # update count of lines
+            count += 1
 
-        Raises:
-            KeyboardInterrupt: If user presses (CTRL + C)
-        """
-        line_count = 0
-        try:
-            for line in sys.stdin:
-                line_count += 1
-                parsed_data = self.parse_line(line)
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
 
-                if parsed_data is not None:
-                    self.total_size += parsed_data["line_size"]
-                    if parsed_data["is_valid_code"]:
-                        total = self.stats.get(parsed_data["status_code"], 0)
-                        self.stats[parsed_data["status_code"]] = total + 1
-                if line_count % 10 == 0:
-                    self.display_stats()
-            self.display_stats()
-        except KeyboardInterrupt:
-            self.display_stats()
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
+except Exception as err:
+    pass
 
-if __name__ == "__main__":
-    parser = LogParser()
-    parser.read_from_stdin()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
